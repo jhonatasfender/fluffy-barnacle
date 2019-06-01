7f8562a6d7be2e39d59be3426b9db7a09e650f47
INTRANET\jonatas.rodrigues <jonatas.rodrigues@faroseducacional.com.br>
2019-06-01 14:28:34 -0300

    update

--- index.js
+++ index.js
@@ -9,29 +9,31 @@ app.set('view engine', 'ejs');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function (request, response) {
+app.get('/', (request, response) => {
   var listSvg = []
-  fs.readdirSync(directory).forEach(file => {
-    if (file.indexOf('html') != -1) {
-      const content = fs.readFileSync(directory + '\\' + file).toString()
-      const split = content.match(/data:image(.*?)("|\&quot\;)/g)
-      if (split) {
-        for (const svg of split) {
-          if (svg.indexOf('&quot;') === -1) {
-            listSvg.push(svg.substring(0, svg.length - 1))
-          } else {
-            listSvg.push(svg.replace('&quot;', ''))
-          }
-        }
-      }
-    }
-  });
+  // fs.readdirSync(directory).forEach(file => {
+  //   if (file.indexOf('html') != -1) {
+  //     const content = fs.readFileSync(directory + '\\' + file).toString()
+  //     const split = content.match(/data:image(.*?)("|\&quot\;)/g)
+  //     if (split) {
+  //       for (const svg of split) {
+  //         if (svg.indexOf('&quot;') === -1) {
+  //           listSvg.push(svg.substring(0, svg.length - 1))
+  //         } else {
+  //           listSvg.push(svg.replace('&quot;', ''))
+  //         }
+  //       }
+  //     }
+  //   }
+  // });
 
   response.render('index.ejs', {
     svg: listSvg
   });
 })
 
-app.listen(app.get('port'), function () {
+
+
+app.listen(app.get('port'), () => {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
